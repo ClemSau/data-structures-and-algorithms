d6416e9c3d2a2e4d2464c0451a6e428f8b0b8a83
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2021-01-24 23:59:33 +0100

    Add problem-7

---
+++ leetcode-problems/problem-7.py
@@ -0,0 +1,27 @@
+# 7. Reversed Integer
+
+def reverse(x: int) -> int:
+        z = list(reversed(str(x)))
+        if set(z) == set(['0']):
+            return 0
+        
+        negative = False
+        if z[-1] == '-':
+            negative = True
+            del z[-1]
+            
+        i = 0
+        while z[i] == '0':
+            i += 1
+            
+        if negative:
+            z = -int(''.join(z[i:]))
+        else:
+            z = int(''.join(z[i:]))
+            
+        if (-2**31 < z) and (z < 2**31 - 1):
+            return z
+        
+        return 0
+
+print(reverse(123))
