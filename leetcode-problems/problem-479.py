c3125ac0f04028f38a3ff82dad1774a3476c5012
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2021-06-01 21:44:53 +0200

    Add problem 479

---
+++ leetcode-problems/problem-479.py
@@ -0,0 +1,11 @@
+# 479.
+
+class Solution(object):
+    def truncateSentence(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        return ' '.join(s.split(" ")[:k])
+        
