47d02461979a04c28dad18b7e9827a0ee5c7ad54
Clément Sauvage <cl.sauvage@yahoo.com>
2021-06-11 11:44:47 +0200

    Add problem-1689

---
+++ leetcode-problems/problem-1689.go
@@ -0,0 +1,13 @@
+ // 1698.
+
+package problem1689
+
+func minPartitions(n string) int {
+    var max byte = '0'
+    for i := 0; i < len(n); i++ {
+        if n[i] > max {
+            max = n[i]
+        }
+    }
+    return int(max - '0')
+}
